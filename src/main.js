fe29a8b18a93dbe71aaa0ccb5debeb01869c4424
wisyananda51-boop <wisyananda51@gmail.com>
2026-01-21 14:06:45 +0700

    update

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import './style.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+import 'swiper/css'
+import Swiper from 'swiper'
+import { Navigation } from 'swiper/modules'
+
+new Swiper('.swiper',{
+    modules:[Navigation],
+    slidesPerView:1,
+    spaceBetween:24,
+    navigation:{
+        nextEl:'.button-next', 
+        prevEl:'.button-prev'
+    },
+    breakpoints:{
+        640:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:4,
+        }
+    },
+    autoplay:{
+        delay:1000,
+    }
+})
+
+new Swiper('.testimoni',{
+    modules:[Navigation],
+    slidesPerView:1,
+    spaceBetween:24,
+    navigation:{
+        nextEl:'.testi-next', 
+        prevEl:'.testi-prev'
+    },
+    breakpoints:{
+        640:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        }
+    },
+    autoplay:{
+        delay:1000,
+    }
+})
